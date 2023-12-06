a759809cd1eda7a7dfc570ed2a4910819102c060
Gurianov1 <newsaabbb228@yandex.ru>
2023-12-06 15:56:15 +0300

    com

--- HouseTasya/app.go
+++ HouseTasya/app.go
@@ -2,42 +2,9 @@ package main
 
 import (
 	"HouseTasya/HouseTasya/house"
-	"fmt"
 )
-
-func myHouse(house house.House) {
-	fmt.Printf("Описание дома:\n")
-	fmt.Printf("Количество комнат: %d\n", house.RoomsCount)
-	fmt.Printf("Количество этажей: %d\n", house.FloorsCount)
-	fmt.Printf("Площадь дома: %.2f кв. м\n", house.Area)
-
-	fmt.Println("Описание членов семьи:")
-	for _, member := range house.FamilyInfo {
-		fmt.Printf("- %s: %s\n", member.Member, member.Name)
-	}
-
-	fmt.Println("Описание родственников:")
-	for _, member := range house.RelativesInfo {
-		fmt.Printf("- %s: %s\n", member.Member, member.Name)
-	}
-
-	fmt.Println("Описание мебели:")
-	for _, furniture := range house.FurnitureInfo {
-		fmt.Printf("- %s: %.2f кв. м, %d шт.\n", furniture.Name, furniture.Size, furniture.Count)
-	}
-
-	fmt.Println("Описание техники:")
-	for _, appliance := range house.ApplianceInfo {
-		fmt.Printf("- %s: %s, %d шт.\n", appliance.Name, appliance.Brand, appliance.Count)
-	}
-
-	fmt.Println("Описание одежды:")
-	for _, clothes := range house.ClothesInfo {
-		fmt.Printf("- %s: %d, %d шт.\n", clothes.Name, clothes.Size, clothes.Count)
-	}
-}
 
 func main() {
 	newHouse := house.CreateHouse()
-	myHouse(newHouse)
+	house.MyHouse(newHouse)
 }
--- HouseTasya/house/House.go
+++ HouseTasya/house/House.go
@@ -6,6 +6,7 @@ import (
 	"HouseTasya/HouseTasya/family"
 	"HouseTasya/HouseTasya/furniture"
 	"HouseTasya/HouseTasya/relatives"
+	"fmt"
 )
 
 type House struct {
@@ -29,5 +30,37 @@ func CreateHouse() House {
 		FurnitureInfo: furniture.AddFurnitureInfo(),
 		ApplianceInfo: appliance.AddApplianceInfo(),
 		ClothesInfo:   clothes.AddClothesInfo(),
+	}
+}
+
+func MyHouse(house House) {
+	fmt.Printf("Описание дома:\n")
+	fmt.Printf("Количество комнат: %d\n", house.RoomsCount)
+	fmt.Printf("Количество этажей: %d\n", house.FloorsCount)
+	fmt.Printf("Площадь дома: %.2f кв. м\n", house.Area)
+
+	fmt.Println("Описание членов семьи:")
+	for _, newObject := range house.FamilyInfo {
+		fmt.Printf("- %s: %s\n", newObject.Member, newObject.Name)
+	}
+
+	fmt.Println("Описание родственников:")
+	for _, newObject := range house.RelativesInfo {
+		fmt.Printf("- %s: %s\n", newObject.Member, newObject.Name)
+	}
+
+	fmt.Println("Описание мебели:")
+	for _, newObject := range house.FurnitureInfo {
+		fmt.Printf("- %s: %.2f кв. м, %d шт.\n", newObject.Name, newObject.Size, newObject.Count)
+	}
+
+	fmt.Println("Описание техники:")
+	for _, newObject := range house.ApplianceInfo {
+		fmt.Printf("- %s: %s, %d шт.\n", newObject.Name, newObject.Brand, newObject.Count)
+	}
+
+	fmt.Println("Описание одежды:")
+	for _, newObject := range house.ClothesInfo {
+		fmt.Printf("- %s: %d, %d шт.\n", newObject.Name, newObject.Size, newObject.Count)
 	}
 }
